ebc66abec820c94c7813d5ba8cb6ca08e3b613a1
Youathor <yprogramming@techcsl.com>
2023-02-23 13:32:47 +0700

    Approve test

--- server/middleware/apiGaurd.ts
+++ server/middleware/apiGaurd.ts
@@ -6,13 +6,13 @@ export default defineEventHandler(async event => {
 	if (url?.startsWith('/api')) {
 		// return send(event, 'No permission')
 		const bToken: any = event.node.req.headers?.btoken
-		if (!bToken) return send(event, 'No permission')
+		if (!bToken) return sendRedirect(event, '/', 302)
 		try {
 			const bytes = CryptoJS.AES.decrypt(bToken, config.cpubKey)
 			const decriptToken = JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
 			console.log(decriptToken)
 			const hash = CryptoJS.enc.Base64.stringify(CryptoJS.HmacSHA256(CryptoJS.enc.Hex.parse(decriptToken?.time?.toString()), config.hpubKey))
-			if (!(decriptToken && hash === decriptToken?.key)) return send(event, 'No permission')
+			if (!(decriptToken && hash === decriptToken?.key)) return sendRedirect(event, '/', 302)
 		} catch (error) {
 			console.log(error)
 			return send(event, 'No permission')
