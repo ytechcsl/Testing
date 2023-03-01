01df704f96311bcd4bdc223cd73a6a6613b31c4c
Youathor <yprogramming@techcsl.com>
2023-03-01 14:04:19 +0700

    Test fetch client middle ware

--- server/middleware/deviceDetection.ts
+++ server/middleware/deviceDetection.ts
@@ -14,25 +14,27 @@ export default defineEventHandler(async event => {
 	event.context.deviceInfo = deviceInfo
 	event.context.auth = { user: 123 }
 
-	// const cookie = parseCookies(event)
-	// console.log('Cookies ', cookie)
-	// const cIp = clientIp
-	// if (!cookie.area) {
-	// 	const areaInfo = await $fetch(`http://ip-api.com/json/${cIp}?fields=66846719`)
-	// 	const encryptArea = CryptoJS.AES.encrypt(JSON.stringify(areaInfo), config.cpriKey).toString()
-	// 	setCookie(event, 'area', encryptArea)
-	// 	// event.context.area = areaInfo
-	// } else {
-	// 	const bytes = CryptoJS.AES.decrypt(cookie.area, config.cpriKey)
-	// 	const areaInfo = JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
-	// 	event.context.area = areaInfo
-	// 	if (cIp !== areaInfo.query) {
-	// 		const areaInfo = await $fetch(`http://ip-api.com/json/${cIp}?fields=66846719`)
-	// 		const encryptArea = CryptoJS.AES.encrypt(JSON.stringify(areaInfo), config.cpriKey).toString()
-	// 		setCookie(event, 'area', encryptArea, {
-	// 			path: '/',
-	// 			maxAge: 60 * 60 * 1
-	// 		})
-	// 	}
-	// }
+	const cookie = parseCookies(event)
+	console.log('Cookies ', cookie)
+	const cIp = clientIp
+	if (!cookie.area) {
+		const areaInfo = await $fetch(`http://ip-api.com/json/${cIp}?fields=66846719`)
+		console.log('Fetch area  ', areaInfo)
+		const encryptArea = CryptoJS.AES.encrypt(JSON.stringify(areaInfo), config.cpriKey).toString()
+		setCookie(event, 'area', encryptArea)
+		event.context.area = areaInfo
+	} else {
+		const bytes = CryptoJS.AES.decrypt(cookie.area, config.cpriKey)
+		const areaInfo = JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
+		console.log('De token ', areaInfo)
+		event.context.area = areaInfo
+		if (cIp !== areaInfo.query) {
+			const areaInfo = await $fetch(`http://ip-api.com/json/${cIp}?fields=66846719`)
+			const encryptArea = CryptoJS.AES.encrypt(JSON.stringify(areaInfo), config.cpriKey).toString()
+			setCookie(event, 'area', encryptArea, {
+				path: '/',
+				maxAge: 60 * 60 * 1
+			})
+		}
+	}
 })
