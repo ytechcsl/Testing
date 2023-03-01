2c4315f37337bb5271e8114fe912c8143d7138b4
Youathor <yprogramming@techcsl.com>
2023-03-01 16:47:46 +0700

    Add custom nuxt3 error page

---
+++ error.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Sorry, Something went wrong please try again after five mimutes</h1>
+        <div>{{ props }}</div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+    error: Object,
+});
+</script>
