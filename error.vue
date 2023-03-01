8963a7c384c6fc99f5fcf646b538658aa0e0204c
Youathor <yprogramming@techcsl.com>
2023-03-01 16:49:23 +0700

    custom error

--- error.vue
+++ error.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
-        <h1>Sorry, Something went wrong please try again after five mimutes</h1>
-        <div>{{ props }}</div>
+    <div class="px-5 pt-5">
+        <h1>Sorry, Something went wrong please try again after 30 seconds</h1>
+        <!-- <div>{{ props }}</div> -->
     </div>
 </template>
 
