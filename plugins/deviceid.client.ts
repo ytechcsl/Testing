3c5123f0c25c24d1d7d86d26a3d9bd5202b1d1ec
Youathor <yprogramming@techcsl.com>
2023-03-01 11:20:14 +0700

    Test location

--- plugins/deviceid.client.ts
+++ plugins/deviceid.client.ts
@@ -1,6 +1,10 @@
 import DeviceDetector from 'device-detector-js'
 export default defineNuxtPlugin(nuxtApp => {
 	const dvid = useCookie<string>('dvid')
+	console.log(navigator)
+	navigator.permissions.query({ name: 'geolocation' }).then(res => {
+		console.log(res)
+	})
 	if (!dvid.value) {
 		const deviceDetector = new DeviceDetector()
 		const deviceInfo = deviceDetector.parse(navigator.userAgent)
--- plugins/location.client.ts
+++ plugins/location.client.ts
@@ -1,5 +1,18 @@
 import { useSetting } from '~/stores/setting'
 export default defineNuxtPlugin(async nuxtApp => {
+	navigator.geolocation.watchPosition(
+		location => {
+			console.log('Watch Access ', location)
+		},
+		error => {
+			console.log('Watch Error ', error)
+		},
+		{
+			enableHighAccuracy: false,
+			timeout: 1000,
+			maximumAge: 0
+		}
+	)
 	navigator.geolocation.getCurrentPosition(
 		position => {
 			// console.log(position.coords)
