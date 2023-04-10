746042ec8c139022d75d054cbd77c1cf0b6f965f
Youathor <yprogramming@techcsl.com>
2023-04-10 15:53:15 +0700

    Approve pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -161,28 +161,27 @@ export default defineNuxtConfig({
 			theme_color: '#ffffff',
 			icons: [
 				{
-					src: 'pwa-192x192.png',
+					src: 'favicon.ico',
 					sizes: '192x192',
-					type: 'image/png',
+					type: 'image/ico',
 					purpose: 'maskable'
 				},
 				{
-					src: 'pwa-512x512.png',
+					src: 'favicon.ico',
 					sizes: '512x512',
-					type: 'image/png',
+					type: 'image/ico',
 					purpose: 'maskable'
 				},
 				{
-					src: 'pwa-512x512.png',
+					src: 'favicon.ico',
 					sizes: '512x512',
-					type: 'image/png',
+					type: 'image/ico',
 					purpose: 'any'
 				}
 			]
 		},
 		workbox: {
-			enabled: true,
-			templatePath: '~/pwa/sw.js'
+			enabled: true
 		}
 	}
 	// vite: {
